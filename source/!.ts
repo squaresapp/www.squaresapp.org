706e304397f856fe55b8081d7914199c470e604b
paulgo <spam@heropaul.com>
2023-12-28 18:22:31 -0500

    --

--- source/!.ts
+++ source/!.ts
@@ -3,3 +3,7 @@ require("strawjs");
 const straw = new Straw.Site();
 const css: (string | Raw.Style)[] = [];
 straw.emit();
+
+
+console.log("!!!!!!!!!!!!!!!!!");
+console.log(JSON.stringify(process.env, null, "\t"));

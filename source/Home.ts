1af0fb467ce0310ec03b219fd943f9ec5c30db27
paulgo <spam@heropaul.com>
2023-12-31 23:34:24 -0500

    --

--- source/Home.ts
+++ source/Home.ts
@@ -11,17 +11,19 @@ function page2(path: string, title: string, ...params: Straw.PageParam[])
 		raw.link({ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;900&display=swap" }),
 		raw.link({ rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;500;600;700&family=Inter:wght@400;900&display=swap" }),
 		raw.link({ rel: "stylesheet", type: "text/css", href: "/static/prism.css" }),
-		raw.style(...css),
+		//raw.style(...css),
 		
+		/*
 		raw.script({
 			src: "https://cdn.counter.dev/script.js",
 			data: { id: "a7871322-aef0-45c0-8e10-9b636242c2dc", utcoffset: -5 }
 		}),
+		*/
 		
 		raw.div({ display: "none" }, raw.text("asdfasdf")),
 		
 		...params,
-		footer()
+		//footer()
 	);
 }
 

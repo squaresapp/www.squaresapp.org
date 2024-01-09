daf7f93063ec30ea0b126deefb7c37b1786b31c0
paulgo <spam@heropaul.com>
2024-01-09 14:44:48 -0500

    --

--- source/WellKnown.ts
+++ source/WellKnown.ts
@@ -1,4 +1,5 @@
 
+// iOS Universal Links file
 straw.file("/.well-known/apple-app-site-association", 
 	JSON.stringify({
 		applinks: {
@@ -11,4 +12,16 @@ straw.file("/.well-known/apple-app-site-association",
 			]
 		},
 	})
+);
+
+// Android Universal Links file
+straw.file("/.well-known/assetlinks.json",
+	JSON.stringify([{
+		relation: ["delegate_permission/common.handle_all_urls"],
+		target : {
+			namespace: "android_app",
+			package_name: "app.squaresapp.org",
+			sha256_cert_fingerprints: ["79:26:BE:31:88:24:75:49:AC:BE:10:E1:D3:C3:9B:42:AE:D9:13:3B:FD:DE:25:5F:2B:A0:C7:A7:C1:0F:C3:9A"]
+		}
+	}])
 );
